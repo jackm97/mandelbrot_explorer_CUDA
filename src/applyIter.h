00bf7d3681374a9f1590c91df4920d91b2397af2
Jack Myers <jackmyers@umail.ucsb.edu>
2019-09-09 00:35:09 -0700

    Inner point checking

--- src/applyIter.h
+++ src/applyIter.h
@@ -1,5 +1,6 @@
 #include "tbb/tbb.h"
 #include <Eigen/Dense>
+
  
 using namespace tbb;
 using Eigen::Array;
@@ -12,13 +13,19 @@ public:
         double iters=0,
                R2=1e6,
                zr2=0,
-               zi2=0;
+               zi2=0,
+	       q;
         for( size_t i=r.cols().begin(); i!=r.cols().end(); ++i ){ 
            for (size_t j=r.rows().begin(); j!=r.rows().end(); ++j){
                         iters=0;
                         zr2=0;
                         zi2=0;
-                        while((zr2+zi2<=R2) && (iters<max_iter)){
+			q = (cr(j,i)-1./4)*(cr(j,i)-1./4) + ci(j,i)*ci(j,i);
+			if (q*(q+(cr(j,i)-1./4)) <= 1./4*ci(j,i)*ci(j,i))
+				iters=max_iter;
+			else if ((cr(j,i)+1)*(cr(j,i)+1) + ci(j,i)*ci(j,i) <= 1./16)
+				iters=max_iter;
+			while((zr2+zi2<=R2) && (iters<max_iter)){
                                 zi(j,i) = zi(j,i) * zr(j,i);
                                 zi(j,i) = zi(j,i) + zi(j,i) + ci(j,i);
                                 zr(j,i) = zr2 - zi2 + cr(j,i);
