907e1ced58d711e7ba5f21562fa40f0fd6e9f05c
jackm97 <jackhmyers97@gmail.com>
2020-02-16 18:45:43 -0800

    changed double to float

--- src/applyIterGPU.cu
+++ src/applyIterGPU.cu
@@ -1,46 +1,10 @@
 #include "applyIterGPU.h"
 
-__global__
-void GPU_PAR_FOR_HELPER(int height, int width,double* values, double* zr, double* zi, double* cr, double* ci, size_t max_iter)
+__device__
+void calcPoint(float& cr, float& ci, float centerx, float centery, float zoom, int width, int height, int i, int j)
 {
-  double iters=0,
-         R2=1e6,
-         zr2=0,
-         zi2=0,
-         q;
-   
-  int i = blockIdx.x*blockDim.x + threadIdx.x;
-  
-  if (i<height*width)
-  {
-    // q is used to determine if a point is within the set
-    // without needing to iterate to max_iter
-    q = (cr[i]-1./4)*(cr[i]-1./4) + ci[i]*ci[i];
-
-    /*if (q*(q+(cr[i]-1./4)) <= 1./4*ci[i]*ci[i])
-      iters=max_iter;
-    else if ((cr[i]+1)*(cr[i]+1) + ci[i]*ci[i] <= 1./16)
-      iters=max_iter;*/
-    while((zr2+zi2<=R2) && (iters<max_iter))
-    {
-      zi[i] = zi[i] * zr[i];
-      zi[i] = zi[i] + zi[i] + ci[i];
-      zr[i] = zr2 - zi2 + cr[i];
-      zr2 = zr[i]* zr[i];
-      zi2 = zi[i]* zi[i]; 
-      iters++;
-    }
-    values[i] = iters;
-  }
-}
-
-__global__
-void SET_COORD_VALS_HELPER(double* zr, double* zi, double* cr, double* ci, double centerx, double centery, double zoom, int width, int height)
-{
-  int i = blockIdx.x*blockDim.x + threadIdx.x;
-
-  double aspect_ratio = double(width)/height;
-  double x_range, y_range, xmin, ymin, intervalx, intervaly;
+  float aspect_ratio = float(width)/height;
+  float x_range, y_range, xmin, ymin, intervalx, intervaly;
   if (aspect_ratio<1){
     x_range = 4/zoom;
     y_range = (1/aspect_ratio)*4/zoom;
@@ -54,17 +18,39 @@ void SET_COORD_VALS_HELPER(double* zr, double* zi, double* cr, double* ci, doubl
   intervalx = x_range/width;
   intervaly = y_range/height;
   
-  double x,y;
-  if (i<width){
-    for (int j=0; j<height; j++){
-      x = xmin + i*intervalx;
-      y = ymin + j*intervaly;
-      //mandelbrot::Point point(xmin + i*intervalx, ymin + j*intervaly);
-      cr[height*i + j] = x;
-      ci[height*i + j] = y;
-      zr[height*i + j] = 0;
-      zr[height*i + j] = 0;
+  cr = xmin + i*intervalx;
+  ci = ymin + j*intervaly;
+}
+
+__global__
+void GPU_PAR_FOR_HELPER(int height, int width,float* values, float centerx, float centery, float zoom, size_t max_iter)
+{
+  float iters=0,
+         R2=10,
+         zr=0,
+         zi=0,
+         cr=0,
+         ci=0,
+         zr2=0,
+         zi2=0;
+   
+  int idx = blockIdx.x*blockDim.x + threadIdx.x;
+  
+  if (idx<height*width)
+  {
+    float i = floorf(idx/height);
+    float j = idx - height*i;
+    calcPoint(cr,ci,centerx,centery,zoom,width,height,i,j);
+    while((zr2+zi2<=R2) && (iters<max_iter))
+    {
+      zi = zi * zr;
+      zi = zi + zi + ci;
+      zr = zr2 - zi2 + cr;
+      zr2 = zr* zr;
+      zi2 = zi* zi; 
+      iters++;
     }
+    values[idx] = iters;
   }
 }
 
@@ -73,39 +59,42 @@ applyIterGPU::applyIterGPU(int height, int width, size_t max_iter):
   width(width),
   max_iter(max_iter)
 {
-  cudaMalloc(&values, height*width*sizeof(double));
-  cudaMalloc(&zr, height*width*sizeof(double));
-  cudaMalloc(&zi, height*width*sizeof(double));
-  cudaMalloc(&cr, height*width*sizeof(double));
-  cudaMalloc(&ci, height*width*sizeof(double));
+  cudaMalloc(&values, height*width*sizeof(float));
+  /*cudaMalloc(&zr, height*width*sizeof(float));
+  cudaMalloc(&zi, height*width*sizeof(float));
+  cudaMalloc(&cr, height*width*sizeof(float));
+  cudaMalloc(&ci, height*width*sizeof(float));*/
 }
 
 applyIterGPU::~applyIterGPU()
 {
   cudaFree(values);
-  cudaFree(zr);
+  /*cudaFree(zr);
   cudaFree(zi);
   cudaFree(cr);
-  cudaFree(ci);
+  cudaFree(ci);*/
 }
 
-void applyIterGPU::SET_COORD_VALS(double centerx, double centery, double zoom)
+void applyIterGPU::SET_COORD_VALS(float centerx, float centery, float zoom)
 {
-  SET_COORD_VALS_HELPER<<<(width+255)/256, 256>>>(zr, zi, cr, ci, centerx, centery, zoom, width, height);
+  this->centerx = centerx;
+  this->centery = centery;
+  this->zoom = zoom;
+  //SET_COORD_VALS_HELPER<<<(width+127)/128, 128>>>(zr, zi, cr, ci, centerx, centery, zoom, width, height);
 }
     
   
 void applyIterGPU::GPU_PAR_FOR()
 {  
 
-  GPU_PAR_FOR_HELPER<<<((height*width)+255)/256, 256>>>(height, width, values, zr, zi, cr, ci, max_iter);
+  GPU_PAR_FOR_HELPER<<<((height*width)+255)/256, 256>>>(height, width, values, centerx, centery, zoom, max_iter);
 
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
 }
 
-void applyIterGPU::copyValues(double* target)
+void applyIterGPU::copyValues(float* target)
 {
-  cudaMemcpy(target, values, height*width*sizeof(double),cudaMemcpyDeviceToHost);
+  cudaMemcpy(target, values, height*width*sizeof(float),cudaMemcpyDeviceToHost);
 }
 
--- src/applyIterGPU.h
+++ src/applyIterGPU.h
@@ -4,16 +4,17 @@
 // Class for running Mandelbrot iteration in parallel
 // within the mandelbrot class
 class applyIterGPU {
-    double *values, *zr, *zi, *cr, *ci;
+    float *values, *zr, *zi, *cr, *ci;
+    float centerx=0,centery=0,zoom=1;
     size_t max_iter;
     int height, width;
 public:  
   
   void GPU_PAR_FOR();
 
-  void SET_COORD_VALS(double centerx, double centery, double zoom);
+  void SET_COORD_VALS(float centerx, float centery, float zoom);
 
-  void copyValues(double* target);
+  void copyValues(float* target);
   
   void setMaxIter(size_t max_iter){this->max_iter=max_iter;}
 
